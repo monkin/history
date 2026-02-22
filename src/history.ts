c72fea5f44aba6e50550b5b1c50559cf2f53fe33
Andrei Monkin <monkin.andrey@gmail.com>
2026-02-22 12:00:41 +0200

    Extend `History` constructor: add `nextId` parameter to compute the next key based on the max key.

--- src/history.ts
+++ src/history.ts
@@ -2,5 +2,8 @@ import type { Entry } from "./entry.ts";
 import type { List } from "./list";
 
 export class History<T extends Entry<string | number, unknown>> {
-    constructor(readonly items: List<T>) {}
+    constructor(
+        readonly items: List<T>,
+        readonly nextId: (maxKey: Entry.Key<T> | undefined) => Entry.Key<T>,
+    ) {}
 }
