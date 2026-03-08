0e9fa942d372065001bfe60c4a6a5fee549cad32
Andrei Monkin <monkin.andrey@gmail.com>
2026-03-08 16:57:48 +0200

    Add constructor and `empty` method to `SnapshotList`

--- src/snapshot-list.ts
+++ src/snapshot-list.ts
@@ -1,4 +1,21 @@
-export class SnapshotList<Id extends string | number, Snapshot> {}
+import { emptyList, type SortedList } from "./sorted-list.ts";
+
+/**
+ * Immutable list of snapshots
+ */
+export class SnapshotList<Id extends string | number, Snapshot> {
+    /** @internal */
+    constructor(
+        private readonly items: SortedList<SnapshotList.Item<Id, Snapshot>>,
+    ) {}
+
+    static empty<Id extends string | number, Snapshot>(): SnapshotList<
+        Id,
+        Snapshot
+    > {
+        return new SnapshotList(emptyList);
+    }
+}
 
 export namespace SnapshotList {
     export type Item<Id extends string | number, Snapshot> = {
